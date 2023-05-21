2ed90e387185992d78c7b7fc6c1be17042512d24
m.gonzaleze <69689894+Maria-Gon@users.noreply.github.com>
2023-05-21 11:58:16 -0500

    level

--- src/ecs/systems/s_player_state.py
+++ src/ecs/systems/s_player_state.py
@@ -1,22 +1,31 @@
+import json
 import esper
 from src.create.prefab_creator_game import create_explosion
 from src.ecs.components.c_lives import CLives
+from src.create.prefab_creator_interface import create_levels_gui
+from src.ecs.components.c_levels import CLevels
+from src.ecs.components.c_play_state import CPlayState
 from src.ecs.components.c_player_state import CPlayerState, PlayerState
 from src.ecs.components.c_surface import CSurface
 from src.ecs.components.c_transform import CTransform
 from src.engine.service_locator import ServiceLocator
 
 
-def system_player_state(world: esper.World, delta_time: float):
+def system_player_state(world: esper.World, delta_time: float, player: dict):
     player_components = world.get_components(
         CSurface, CTransform, CPlayerState)
+    play_state_components = world.get_component(CPlayState)
     lives_gui_components = world.get_component(CLives)
+    level_gui_components = world.get_component(CLevels)
 
     c_s: CSurface
     c_t: CTransform
     c_pstate: CPlayerState
     for _, (c_s, c_t, c_pstate) in player_components:
         if c_pstate.state == PlayerState.DEAD:
+            player["level"] =  1
+            with open("assets/cfg/player.json", 'w') as archivo_nuevo:
+                json.dump(player, archivo_nuevo, indent=4)
             c_pstate.respawn_time += delta_time
             if c_pstate.respawn_time > 3 and c_pstate.lives > -1:
                 player_cfg = ServiceLocator.config_service.get(
