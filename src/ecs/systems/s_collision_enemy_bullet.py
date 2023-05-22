cddced326a1aecb02511f7b64993e690a5d78029
Juan Romero <juan.romero1201@gmail.com>
2023-05-22 10:44:48 -0500

    Refactor: Delete unused importes

--- src/ecs/systems/s_collision_enemy_bullet.py
+++ src/ecs/systems/s_collision_enemy_bullet.py
@@ -1,6 +1,5 @@
 import esper
 from src.ecs.components.c_score import CScore
-#from src.ecs.components.c_score import CScore
 from src.ecs.components.c_surface import CSurface
 from src.ecs.components.c_transform import CTransform
 from src.ecs.components.tags.c_tag_enemy import CTagEnemy
