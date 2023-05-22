58ab2b37647fa148de3fad1102836f87079fed49
Juan Romero <juan.romero1201@gmail.com>
2023-05-22 11:34:32 -0500

    Feat: Add asynchronous execution to allow the game to be played on the web

--- src/engine/game_engine.py
+++ src/engine/game_engine.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import pygame
 
@@ -36,7 +37,7 @@ class GameEngine:
         self._scene_name_to_switch:str = None
         self._scene_kwargs_to_switch:dict = None
 
-    def run(self, start_scene_name:str) -> None:
+    async def run(self, start_scene_name:str) -> None:
         self.is_running = True
         self._current_scene = self._scenes[start_scene_name]
         self._create()
@@ -46,6 +47,7 @@ class GameEngine:
             self._update()
             self._draw()
             self._handle_switch_scene()
+            await asyncio.sleep(0)
         self._do_clean()
 
     def switch_scene(self, new_scene_name:str, **kwargs):
